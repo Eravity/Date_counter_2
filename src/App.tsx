fe693a8a26613facddff57f311cb814ad3ca8436
Eravity <cebotaricristian92@gmail.com>
2024-03-24 17:06:18 +0200

    v1.0.0

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import Counter from "./assets/components/counter";
 
 const App: React.FC = (): JSX.Element => {
   return (
-    <div className="w-1/6 flex flex-col gap-2 py-5 px-2 h-screen w-screen items-center justify-center">
+    <div className="flex flex-col gap-2 py-5 px-2 h-screen w-screen items-center justify-center">
       <Counter />
     </div>
   );
